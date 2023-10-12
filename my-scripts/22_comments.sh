6da1713a9bf146087002bc144a274c1088b85d5e
jignyasamishra <iamjignyasa@gmail.com>
2023-10-12 23:29:05 +0530

    new code
    Signed-off-by: jignyasamishra <iamjignyasa@gmail.com>

---
+++ my-scripts/22_comments.sh
@@ -0,0 +1,16 @@
+
+
+
+
+#!bin/bash
+
+echo "comments"
+
+#this is a commen
+<<comment
+this
+is
+multi
+line
+comments
+comment
---
+++ my-scripts/33_vardemo.sh
@@ -0,0 +1,19 @@
+
+
+
+
+#!bin/bash
+
+
+
+#script to show how to use variables
+
+a=10
+name="jigs"
+age=22
+
+
+
+
+
+echo "my name is $name and my age is $age"
