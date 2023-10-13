eb520d6d11124d081484bedd44ae1a89a3d567cc
jignyasamishra <iamjignyasa@gmail.com>
2023-10-13 19:23:13 +0530

    new code
    Signed-off-by: jignyasamishra <iamjignyasa@gmail.com>

--- my-scripts/33_vardemo.sh
+++ my-scripts/33_vardemo.sh
@@ -1,79 +1,16 @@
-
-
-
-
 #!bin/bash
-
-
-
 #script to show how to use variables
 
 a=10
 name="jigs"
 age=22
 
-
-
-
-
-
-
-
-
-echo "my name is $name and my age is$age"
+echo "my name is $name and my age is $age"
 
 
 name="jignyasa"
 echo "my name is $name"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-echo "my name is $name"
 
 
 
